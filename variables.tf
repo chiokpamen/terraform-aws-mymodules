679a7c532c485ad5f41c109ec032dc70d38987de
chiokpamen <chi.okpamen@gmail.com>
2022-07-19 09:01:20 -0400

    terraform module

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-04aaa9bae0e375e7b"
+}
+variable "amiID" {
+	default="ami-0ba62214afa52bec7"
+}
+variable "keyPair" {
+	default="myohio-key"
+}
+variable "keyPair1" {
+	default="myohio-key.pem"
+}
+
+
+
+
